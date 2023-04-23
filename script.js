29e156e36bb1863a9e9dff51142fbcb18854e8a2
Rahul sharma <120250301+Rahulsharma0223@users.noreply.github.com>
2023-04-24 00:06:17 +0530

    init

---
+++ script.js
@@ -0,0 +1,109 @@
+console.log("Welcome to Rythm");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    { songname: "AADAT_-_NINJA__PARMISH_VERMA",filePath:"songs/1.mp3",coverpath:"covers/1.jpg"},
+    { songname: "Aaj_Bhi_(Official_Video)",filePath:"songs/2.mp3",coverpath:"covers/2.jpg"},
+    { songname: "Banjaara__cover",filePath:"songs/3.mp3",coverpath:"covers/3.jpg"},
+    { songname: "_Chiggy_Wiggy__Blue___Kylie",filePath:"songs/4.mp3",coverpath:"covers/4.jpg"},
+    { songname: "_Fiqrana__Blue__",filePath:"songs/5.mp3",coverpath:"covers/5.jpg"},
+    { songname: "_Lambiyaan_Si_Judaiyaan",filePath:"songs/6.mp3",coverpath:"covers/6.jpg"},
+    { songname: "_Mitti_Di_Khushboo",filePath:"songs/7.mp3",coverpath:"covers/7.jpg"},
+    { songname: "_MEETHI_MEETHI_GALLAN",filePath:"songs/8.mp3",coverpath:"covers/8.jpg"},
+    { songname: "_Maninder_Buttar",filePath:"songs/9.mp3",coverpath:"covers/9.jpg"},
+    { songname: "_NA_JA_TU",filePath:"songs/10.mp3",coverpath:"covers/10.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
